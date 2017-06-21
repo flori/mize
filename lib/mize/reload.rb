7d9c31b6b27d6ba01bc8d6e8b2dc4293fe813ba0
Florian Frank <flori@ping.de>
2017-06-21 09:17:48 +0200

    Avoid problem during rails development
    Clear wrapped cache if reload! was called to avoid wasting memory.

--- lib/mize/reload.rb
+++ lib/mize/reload.rb
@@ -2,11 +2,14 @@ module Mize::Reload
   # Reload this object if a #reload method was provided up somewhere in the
   # inheritance tree, afterwards clear the memoization cache and return self.
   def reload(*)
-    result = self
-    if defined? super
-      result = super
+    Mize::MUTEX.synchronize do
+      result = self
+      if defined? super
+        result = super
+      end
+      Mize.wrapped.clear
+      mize_cache_clear
+      result
     end
-    mize_cache_clear
-    result
   end
 end
